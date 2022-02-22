2d4f9f0f07074572190292f0ec4c36e49284a2f8
14385423 <alex14385423@outlook.com>
2022-02-22 22:58:43 +0800

    input copyright

--- Wheat/Wheats-Count/utils/trainer.py
+++ Wheat/Wheats-Count/utils/trainer.py
@@ -1,15 +1,27 @@
-'''
-Copyright (c) 2022  https://github.com/14385423  https://github.com/ZCFzhaochuanfei  All rights reserved.
+########################################################################
+# This software is Copyright 2022 The Regents of Shandong Agricultural University.
+# All Rights Reserved.
+#
+# Permission to copy, modify, and distribute this software and its
+# documentation for educational, research and non-profit purposes, without fee,
+# and without a written agreement is hereby granted, provided that the above
+# copyright notice, this paragraph and the following two paragraphs appear
+# in all copies.
+#
+# This software program and documentation are copyrighted by The Regents of
+# Shandong Agricultural University. The software program and documentation are supplied
+# "as is", without any accompanying services from The Regents. The Regents does
+# not warrant that the operation of the program will be uninterrupted or
+# error-free. The end-user understands that the program was developed for
+# research purposes and is advised not to rely exclusively on the program for
+# any reason.
+#
 
-Reference source:
+#Author: Qikun Zhao, Chuanfeng Zhao (https://github.com/14385423,https://github.com/ZCFzhaochuanfei)
+#For commermical usage, please contact corresponding author (johnsunx1@yahoo.com)
+########################################################################
 
-https://github.com/ZhihengCV/Bayesian-Crowd-Counting
 
-https://github.com/dyh/unbox_yolov5_deepsort_counting
-
-https://github.com/ultralytics/yolov5
-
-'''
 
 
 import os
